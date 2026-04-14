35e740fa4042fa46a390519f0bdf7b91b84dc270
ynfleozin <leonardoalvarengam@gmail.com>
2026-04-14 18:36:41 -0300

    feat: add expense repository interface

---
+++ backend/src/main/java/com/leonardoalvarenga/nexcash/repository/ExpenseRepository.java
@@ -0,0 +1,10 @@
+package com.leonardoalvarenga.nexcash.repository;
+
+import com.leonardoalvarenga.nexcash.domain.Expense;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ExpenseRepository extends JpaRepository<Expense, UUID> {
+
+}
