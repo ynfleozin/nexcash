321ab3c48a24ca97d875c24f47cf70bcbfd64d9b
ynfleozin <leonardoalvarengam@gmail.com>
2026-04-14 18:59:44 -0300

    feat: add controller with create expense endpoint

---
+++ backend/src/main/java/com/leonardoalvarenga/nexcash/controller/ExpenseController.java
@@ -0,0 +1,24 @@
+package com.leonardoalvarenga.nexcash.controller;
+
+import com.leonardoalvarenga.nexcash.domain.Expense;
+import com.leonardoalvarenga.nexcash.service.ExpenseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/expenses")
+@RequiredArgsConstructor
+public class ExpenseController {
+    private final ExpenseService service;
+
+    @PostMapping
+    public ResponseEntity<Expense> create(@RequestBody  Expense expense){
+        Expense savedExpense = service.createExpense(expense);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedExpense);
+    }
+}
