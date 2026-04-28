6bee932dc57500890937afe00c5c91610eafe0e5
ynfleozin <leonardoalvarengam@gmail.com>
2026-04-27 21:44:36 -0300

    feat: create premium login UI and wire up authentication simulation

--- frontend/src/app/features/auth/login/login.component.ts
+++ frontend/src/app/features/auth/login/login.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../../core/services/auth.service';
+import { Router } from '@angular/router';
+import { ToastService } from '../../../core/services/toast.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  private authService = inject(AuthService);
+  private router = inject(Router);
+  private toastService = inject(ToastService);
 
+  simulateLogin(role: 'USER' | 'MANAGER') {
+    this.authService.login(role);
+    this.toastService.show(
+      `Welcome back, ${role.toLocaleLowerCase()}!`,
+      'success',
+    );
+
+    const destination = role === 'USER' ? '/user' : '/manager';
+    this.router.navigate([destination]);
+  }
 }
